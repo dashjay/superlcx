f482bda5908daeecce5fd2969e8716583acf61da
DashJay <dashjay@aliyun.com>
2020-07-05 22:28:47 +0800

    update scripts to build all plugins

--- build_all_plugins.sh
+++ build_all_plugins.sh
@@ -1,3 +1,8 @@
-for line in $(ls plugins); \
-  do go build -o $line.so --buildmode=plugin plugins/$line/handler.go;  \
+set -x;
+
+for line in $(ls middlewares); \
+  do awk '{if($0 ~ /^package .*?$/) print "package main"} {if($0 !~ /^package .*?/) print $0}' \
+  middlewares/$line/handler.go >> temp.go; \
+  go build -o $line.so --buildmode=plugin temp.go; \
+  rm temp.go;  \
 done
